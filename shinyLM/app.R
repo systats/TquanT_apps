d27738e243707a2440916d9e04afe6b3b2687d4d
Simon Roth <systats@users.noreply.github.com>
2018-02-24 00:50:13 +0100

    go fabio

--- shinyLM/app.R
+++ shinyLM/app.R
@@ -1,14 +1,38 @@
-# devtools::install_github("Appsilon/semantic.dashboard")
-# devtools::install_github("rstudio/shiny")
-
 library(shiny)
-library(shiny.semantic)
-library(semantic.dashboard)
-library(shinyWidgets)
-library(dplyr)
-library(sjPlot)
-library(flexdashboard)
-library(scatterD3)
+### once install package loader called pacman
+# install.packages("pacman")
+
+### install & load libraries from CRAN
+pacman::p_load(
+  shiny, sjPlot, 
+  dplyr, scatterD3, 
+  plotly, reshape2
+)
+
+### install & load from github (dev versions)
+pacman::p_load_gh(
+  "Appsilon/shiny.semantic",
+  "Appsilon/semantic.dashboard", 
+  "daattali/shinyjs",
+  dependencies = T#, update = T
+)
+# devtools::install_github("Appsilon/shiny.semantic", ref = "develop")
+# devtools::install_github("nteetor/dull")
+
+### for shiny server
+# library(shiny)
+# library(shiny.semantic)
+# library(semantic.dashboard)
+# library(shinyWidgets)
+# library(dplyr)
+# library(sjPlot)
+# library(flexdashboard)
+# library(scatterD3)
+# library(sjPlot)
+# library(plotly)
+# library(reshape2)
+
+source("helper.R")
 
 my_dashboard_header <- function(...){
   # verify_value_allowed("color", ALLOWED_COLORS)
@@ -47,10 +71,10 @@ ui <- dashboardPage(
       )
     )
   ),
-  #dashboardBody(
-  shiny::div(
-    class = "pusher",
-    style="min-height: 100vh;",
+  dashboardBody(
+  # shiny::div(
+  #   class = "pusher",
+  #   style="min-height: 100vh;",
     tabItems(
       selected = 1,
       tabItem(
@@ -58,13 +82,17 @@ ui <- dashboardPage(
         column(
           width = 11,
           box(
-            title = "Sample box", 
+            title = "Scatterplot", 
             color = "blue", 
             title_side = "top", 
             ribbon = F,
             collapsible = F, 
             width = 11,
-            scatterD3::scatterD3Output("plot", width = "100%")
+            br(),
+            br(),
+            scatterD3::scatterD3Output("plot", width = "100%"),
+            br(),
+            br()
           )
         ),
         column(
@@ -76,26 +104,56 @@ ui <- dashboardPage(
             ribbon = F,
             collapsible = T, 
             width = 5,
-            sliderInput("intercept", label = "Intercept", min = -10, max = 10, value = 0, step = 1, animate = T),
-            sliderInput("slope", label = "Slope", min = -2, max = 2, value = 1, step = .1, animate = T),
-            sliderInput("error", label = "RSE", min = 0, max = 10, value = 2, step = .1, animate = T),
-            sliderInput("n", label = "N", min = 10, max = 1000, step = 20, value = 100, animate = T)
+            sliderInput("intercept", label = "Intercept", min = -10, max = 10, value = 0, step = 1, 
+                        animate = animationOptions(playButton = uiicon("green play"), pauseButton = uiicon("red pause"))),
+            sliderInput("slope", label = "Slope", min = -2, max = 2, value = 1, step = .1, 
+                        animate = animationOptions(playButton = uiicon("green play"), pauseButton = uiicon("red pause"))),
+            sliderInput("error", label = "Sigma", min = 0, max = 10, value = 2, step = .5, 
+                        animate = animationOptions(playButton = uiicon("green play"), pauseButton = uiicon("red pause"))),
+            sliderInput("n", label = "N", min = 10, max = 1000, step = 20, value = 100, 
+                        animate = animationOptions(playButton = uiicon("green play"), pauseButton = uiicon("red pause")))
           ),
           box(
             title = "Estimation", 
-            color = "orange", 
+            color = "red", 
             title_side = "top", 
             ribbon = F,
             collapsible = T, 
             width = 4,
-            uiOutput("lm1"),   
-            flexdashboard::gaugeOutput("box")
+            uiOutput("lm1"),
+            br(),
+            uiOutput("ind")
           )
         )
       ),
       tabItem(
         tabName = "multi_lin_reg",
-        ""
+        column(
+          width = 11,
+          box(
+            title = "3D Scatterplot", 
+            color = "blue", 
+            title_side = "top", 
+            ribbon = F,
+            collapsible = F, 
+            width = 11,
+            style="min-height: 100vh;",
+            br(),
+            plotly::plotlyOutput("D3")
+          )
+        ),
+        column(
+          width = 5,
+          box(
+            title = "Parameter", 
+            color = "red", 
+            title_side = "top", 
+            ribbon = F,
+            collapsible = T, 
+            width = 5, 
+            sliderInput("slope1", label = "slope1", min = -2, max = 2, value = 0, step =.1)
+          )
+        )
       )
     )
   )
@@ -106,47 +164,94 @@ server <- function(input, output) {
   dat <- reactive({
     set.seed(2017)
     x <- runif(input$n, 1, 7)
-    error <- rnorm(input$n, sd = input$error)
-    y <- input$intercept + input$slope * x + error
-    #y <- 2 + 2 * x + rnorm(100)
+    y <- input$intercept + input$slope * x + rnorm(input$n, sd = input$error)
     return(data.frame(x, y))
   })
   
-  #broom::glance(lm(y ~ x, dat()))$r.squared
-  output$box <- flexdashboard::renderGauge({
-    # dat() %>% 
-    #   lm(y ~ x, .) %>%
-    #   broom::glance() %>%
-    #   mutate(r.squared = r.squared *100 %>% round(1)) %>% 
-    #   gauge(.$r.squared, min = 0, max = 100, symbol = '%', label = paste("R squared"))
-    gauge(round(broom::glance(lm(y ~ x, dat()))$r.squared*100,1), min = 0, max = 100, symbol = '%', label = paste("R squared"))
+  ind <- reactive({
+    round(broom::glance(lm(y ~ x, dat()))$r.squared*100,1) %>% as.character()
   })
+  
+  output$ind <- renderUI({
+    tagList(
+      div(class="ui green progress",
+          #`data-value` = "1",
+          `data-total` = "100",
+          id = "example5",
+          div(class="bar",
+              div(class="progress")
+          ),
+          div(class="label",
+              "Variance 'Explained'"
+          )
+      ),
+      tags$script(paste0("$('#example5').progress({percent:", ind(), "});"))
+    )
+  })
+  
   
   #output$lm1 <- renderUI(HTML(stargazer::stargazer(lm(y ~ x, dat()), type="html")))
   output$lm1 <- renderUI({
     ##nn <- data.frame(x = rnorm(100), y = rnorm(100)) %>%
     nn <- dat() %>%
       lm(y ~ x, .) %>%
-      sjt.lm(show.std = T, show.se = F, show.ci = F, use.viewer = F)
-    
-    nn$knitr %>% 
+      sjt.lm(
+        show.std = T,
+        show.se = F,
+        show.ci = F, 
+        emph.p = T, 
+        p.zero = F,
+        use.viewer = F, 
+        cell.spacing = .25
+      )
+
+    nn$knitr %>%
       HTML()
+    #nn <- data.frame(x = rnorm(100), y = rnorm(100))
+    # dat() %>%
+    #   lm(y ~ x, .) %>%
+    #   broom::tidy(.) %>%
+    #   rename(` ` = term, B = estimate, SE = std.error, p = p.value) %>%
+    #   select(-statistic) %>%
+    #   mutate(B = round(B, 2), SE = round(SE, 2), p = round(p, 2)) %>%
+    #   mutate(p = kableExtra::cell_spec(p, "html", color = ifelse(p < .05, "green", "red"))) %>%
+    #   knitr::kable(format = "html", digits = 2) %>%
+    #   HTML()
   })
   
   output$plot <- scatterD3::renderScatterD3({
     library(scatterD3)
-    scatterD3(data = dat(), x = x, y = y, 
-              lines = data.frame(
-                slope = input$slope, 
-                intercept = input$intercept
-              ), 
-              transitions = T)
+    scatterD3(
+      data = dat(), 
+      x = x, y = y, 
+      lines = data.frame(
+        slope = input$slope, 
+        intercept = input$intercept
+      ), 
+      transitions = T
+    )
     #hchart(dat, "point", hcaes(x, y))  %>%
     #hc_add_series(predictions, type = "line")
   })
   
-  output$vis <- renderText({ "Hello Second Tab :D"})
+  dat_3d <- reactive({
+    x1 <- runif(100, min = 0, max = 10)
+    hist(x1)
+    
+    x2 <- runif(100, min = 0, max = 10)
+    
+    y <- as.numeric(input$slope1) * x1 + x2 + rnorm(100) 
+    y <- y/max(y)*10
+
+    return(data.frame(x1, x2, y))
+  })
   
+  
+  output$D3 <- plotly::renderPlotly({
+    fit_plane_surface <- fit_plane(dat_3d())
+    p <- plot_plane(dat_3d(), fit_plane_surface)
+    return(p)
+  })
 }
 
 shinyApp(ui, server)
---
+++ shinyLM/helper.R
@@ -0,0 +1,52 @@
+fit_plane <- function(data){
+  
+  fit_plane <- lm(y ~ 1 + x1 + x2, data = data)
+  
+  #Graph Resolution (more important for more complex shapes)
+  graph_reso <- 0.05
+  
+  #Setup Axis
+  axis_x1 <- seq(0, 10, by = graph_reso)
+  axis_x2 <- seq(0, 10, by = graph_reso)
+  
+  #Sample points
+  fit_plane_surface <- expand.grid(
+    x1 = axis_x1,
+    x2 = axis_x2,
+    KEEP.OUT.ATTRS = F
+  )
+  
+  fit_plane_surface$y <- predict.lm(fit_plane, newdata = fit_plane_surface)
+  fit_plane_surface <- reshape2::acast(fit_plane_surface, x2 ~ x1, value.var = "y")
+  
+  return(fit_plane_surface)
+}
+
+plot_plane <- function(data, surface){
+
+  graph_reso <- 0.05
+  axis_x1 <- seq(0, 10, by = graph_reso)
+  axis_x2 <- seq(0, 10, by = graph_reso)
+  
+  p <- plotly::plot_ly(
+    dat,
+    x = ~ x1,
+    y = ~ x2,
+    z = ~ y,
+    type = "scatter3d",
+    mode = "markers", 
+    height = 700,
+    width = 700
+  ) %>%
+  plotly::add_trace(
+    p = .,
+    z = fit_plane_surface,
+    x = axis_x1,
+    y = axis_x2,
+    type = "surface", 
+    showlegend = F
+  ) %>%
+  plotly::layout(showlegend = F) 
+  
+  return(p)
+}
